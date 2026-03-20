081bf08b2f7a16b047d2f7cfad2b0ed9a2a8a9ff
TeodorHutanu <teodor.hutanu4@gmail.com>
2026-03-20 17:47:57 +0200

    second

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,14 +2,24 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class UserController
+class UserController extends AbstractController
 {
-    #[Route('/')]
-    public function homepage()
+    #[Route("/")] 
+    public function homepage(): Response 
     {
-        return new Response('<strong> Starshop</strong>: your monopoly-busting options for Starships parts ');
+        $starshipsCount = 457;
+        $myship=[
+            "name" => "USS Leaf",
+            "color" => "green",
+            "status" => "MIA"
+         ];
+
+        return $this->render("main/homepage.html.twig", ["numberofStarships" => $starshipsCount, "myShip" =>  $myship,]);
+        
+
     }
 }
